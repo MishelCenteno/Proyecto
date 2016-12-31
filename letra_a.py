d5cd4a3ca891ef660dc6aae19831a4eb553440c7
MishelCenteno <mishelcenteno96@hotmail.com>
2016-12-31 16:07:08 -0500

    Letra A

---
+++ letra_a.py
@@ -0,0 +1,34 @@
+from turtle import *
+import turtle
+t = turtle.Pen()
+
+t.left(80)
+t.forward(180)
+
+t.left(280)
+t.forward(40)
+
+t.left(280)
+t.forward(180)
+
+t.left(260)
+t.forward(20)
+
+t.left(280)
+t.forward(70)
+
+t.left(80)
+t.forward(40)
+
+t.left(80)
+t.forward(70)
+
+t.left(280)
+t.forward(20)
+penup()
+
+goto(30,92)
+pendown()
+goto(70,92)
+goto(51,160)
+goto(30,92)
