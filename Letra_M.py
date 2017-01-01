118290be491d644e0a149d33fd1f632e5c474bf2
Pc-PC\Pc <fertibanta2806@gmail.com>
2017-01-01 15:07:12 -0500

    Letra M

---
+++ Letra_M.py
@@ -0,0 +1,16 @@
+import turtle
+t=turtle.Pen()
+
+t.pencolor("sky blue")
+t.pensize(12)
+
+t.left(90)
+t.forward(150)
+t.right(135)
+t.forward(100)
+t.right(270)
+t.forward(100)
+t.right(135)
+t.forward(150)
+
+
